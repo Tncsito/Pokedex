38fcafe114195ac92a623f6ebcf8ed171b252af2
Tncsito <eduarosantoyo@gmail.com>
2023-11-15 22:19:48 -0700

    Agregar archivos de proyecto.

---
+++ Pokedex_EESA/Datos/Dpokemon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pokedex_EESA.Modelo;
+using Pokedex_EESA.Conexion;
+using Firebase.Database.Query;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Pokedex_EESA.Datos
+{
+    public class Dpokemon
+    {
+        public async Task Insertarpokemon(Mpokemon parametros)
+        {
+            await Cconexion.firebase.Child("Pokemon").PostAsync(new Mpokemon()
+            {
+                 ColorFondo = parametros.ColorFondo,
+                 Colorpoder = parametros.Colorpoder,
+                 Icono = parametros.Icono,
+                 Nombre = parametros.Nombre,
+                 Nroorden = parametros.Nroorden,
+                 Poder = parametros.Poder,
+            });
+        }
+        public async Task<List<Mpokemon>> MostrarPokemones()
+        {
+            return (await Cconexion.firebase.Child("Pokemon").OnceAsync<Mpokemon>()).Select(item => new Mpokemon
+            {
+                Idpokemon = item.Key,
+                Nombre = item.Object.Nombre,
+                ColorFondo = item.Object.ColorFondo,
+                Colorpoder = item.Object.Colorpoder,
+                Nroorden = item.Object.Nroorden,
+                Poder = item.Object.Poder,
+                Icono = item.Object.Icono
+            }).ToList();
+        }
+    }
+}
---
+++ Pokedex_EESA/VistaModelo/VMPokemon/VMlistapokemon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Pokedex_EESA.Vista.Pokemon;
+
+namespace Pokedex_EESA.VistaModelo.VMPokemon
+{
+    public class VMlistapokemon:BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+
+        #region CONTRUCTOR
+
+        #endregion
+
+        #region OBJETOS
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+
+        #region PROCESOS
+        public async Task Iraregistro()
+        {
+            await Navigation.PushModalAsync(new RegistrarPokemon());
+        }
+
+        public void ProcesoSimple()
+        {
+        }
+        #endregion
+
+        #region COMANDOS
+        public ICommand Iraregistrocommand => new Command(async () => await Iraregistro());
+        public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        #endregion
+
+        #region CONTRUCTOR
+        public VMlistapokemon(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+    }
+}
